621eddc4754432f402ed5158e3974ec5b00e4c2c
PlehanovTA <artjom.plehanov@tptlive.ee>
2022-03-24 11:31:44 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,7 +5,85 @@ if next_position == "1":
     print(". . .")
     print(". . .")
 
-if next_position == "2":
+    next_position = input("Please type what position: ")
+
+    if next_position == "1":
+        print("CAN'T DO THE MOVE")
+
+    elif next_position == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
+
+        next_position = input("Please type what position: ")
+
+        if next_position == "1":
+            print("CAN'T DO THE MOVE")
+
+        elif next_position == "2":
+            print("CAN'T DO THE MOVE")
+
+        elif next_position == "3":
+            print("x o x")
+            print(". . .")
+            print(". . .")
+    
+    elif next_position == "3":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+        next_position = input("Please type what position: ")
+
+        if next_position == "1":
+            print("CAN'T DO THE MOVE")
+
+        elif next_position == "2":
+            print("CAN'T DO THE MOVE")
+
+        
+
+elif next_position == "2":
     print(". x .")
     print(". . .")
     print(". . .")
+
+elif next_position == "3":
+    print(". . x")
+    print(". . .")
+    print(". . .")
+
+elif next_position == "4":
+    print(". . .")
+    print("x . .")
+    print(". . .")
+
+elif next_position == "5":
+    print(". . .")
+    print(". x .")
+    print(". . .")
+
+
+elif next_position == "6":
+    print(". . .")
+    print(". . x")
+    print(". . .")
+
+
+elif next_position == "7":
+    print(". . .")
+    print(". . .")
+    print("x . .")
+
+
+elif next_position == "8":
+    print(". . .")
+    print(". . .")
+    print(". x .")
+
+
+elif next_position == "9":
+    print(". . .")
+    print(". . .")
+    print(". . x")
+
