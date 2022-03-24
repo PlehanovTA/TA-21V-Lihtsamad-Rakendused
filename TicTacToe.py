b27ab3391a4d0753604cdc935358d933d450bb73
PlehanovTA <artjom.plehanov@tptlive.ee>
2022-03-24 11:28:12 +0200

    Update TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,11 @@
+next_position = input("Please type what position: ")
+
+if next_position == "1":
+    print("x . .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "2":
+    print(". x .")
+    print(". . .")
+    print(". . .")
